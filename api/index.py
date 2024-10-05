f74641f669b1c7b74de70af5dbacbbcc0aaed73d
kiriland <27444755+kiriland@users.noreply.github.com>
2024-10-05 19:08:51 -0400

    vercel

--- api/index.py
+++ api/index.py
@@ -3,7 +3,26 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(
+ app,
+ resources={
+  r"/*": {
+   "origins": "*",
+   "methods": [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+   ],
+   "allow_headers": [
+    "Content-Type",
+    "Authorization",
+   ],
+  }
+ },
+)
 
 @app.route("/api/python")
 def get_data():

e45ec021ecda948290a6d9e7a745295264ce716f
kiriland <27444755+kiriland@users.noreply.github.com>
2024-10-05 19:55:35 -0400

    vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,24 @@ export const dynamic = 'force-dynamic'
 export const revalidate = 0;
 
 export default async function HomePage() {
-  try {
-    const aiResponse = await getMyAIResponce();
+  const aiResponse = await getMyAIResponce();
+  if (aiResponse) {
     // Handle successful response
     return (
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <UploadSection/>
         {aiResponse.name}
-      </main>)
-  } catch (error) {
-    console.error("Failed to get AI response:", error);
+      </main>
+    );
+  } else {
+    console.error("Failed to get AI response");
     // Handle the error appropriately in your UI
     return (
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <UploadSection/>
         <p>Failed to get AI response</p>
-      </main>)
+      </main>
+    );
   }
  
   

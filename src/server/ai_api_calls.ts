9a2d6bc7e52fa251f4e8b0ddd4cd2221793b54a1
kiriland <27444755+kiriland@users.noreply.github.com>
2024-10-05 18:34:26 -0400

    vercel pls

--- src/server/ai_api_calls.ts
+++ src/server/ai_api_calls.ts
@@ -1,10 +1,19 @@
 import "server-only";
 
 const URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
-  : "http://localhost:5328/api";
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/ai/api`
+  : "http://localhost:5328/ai/api";
 
 export async function getMyAIResponce(){
-    let data = (await fetch(`${URL}/ai/python`)).json()
-    return data
+    try {
+        const response = await fetch(`${URL}/python`, {
+          method: "GET",
+        });
+        const data = await response.json();
+        return data
+      } catch (error) {
+        console.error("Error creating todo:", error);
+      }
+    
 }
+

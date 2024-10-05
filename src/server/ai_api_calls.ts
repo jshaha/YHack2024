76c4616dbae35d52a646b6e6176fbfc9e53ef64a
kiriland <27444755+kiriland@users.noreply.github.com>
2024-10-05 18:25:30 -0400

    vercel pls

--- src/server/ai_api_calls.ts
+++ src/server/ai_api_calls.ts
@@ -1,8 +1,8 @@
 import "server-only";
 
 const URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
-  : "http://localhost:5328/api";
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/ai`
+  : "http://localhost:5328/api/ai";
 
 export async function getMyAIResponce(){
     let data = (await fetch(`${URL}/ai/python`)).json()

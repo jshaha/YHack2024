7e0ec48bfe62807f30ce53fd6ca6f2f9643fbcab
kiriland <27444755+kiriland@users.noreply.github.com>
2024-10-05 18:52:46 -0400

    vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,11 @@ export const revalidate = 0;
 
 export default async function HomePage() {
   const data = await getMyAIResponce()
-  const URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/ai`
-  : "http://localhost:5328/api/ai";
-
+ 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Hello World!</h1>
       <UploadSection/>
-      <p>{URL}</p>
+      {data?.name}
     </main>
   );
 }

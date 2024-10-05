aa2ab7f10d9cf81d93947f6fecde7b00dffd1ce2
kiriland <27444755+kiriland@users.noreply.github.com>
2024-10-05 16:00:45 -0400

    database connected

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,6 +4,7 @@
 import { sql } from "drizzle-orm";
 import {
   index,
+  integer,
   pgTableCreator,
   serial,
   timestamp,
@@ -18,11 +19,13 @@ import {
  */
 export const createTable = pgTableCreator((name) => `yhack2024_${name}`);
 
-export const posts = createTable(
-  "post",
+export const slides = createTable(
+  "slide",
   {
     id: serial("id").primaryKey(),
     name: varchar("name", { length: 256 }),
+    url: varchar("url", { length: 1024 }).notNull(),
+    userId: varchar("user_id", { length: 256 }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
@@ -30,7 +33,18 @@ export const posts = createTable(
       () => new Date()
     ),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+);
+export const items = createTable(
+  "item",
+  {
+    page: serial("page").primaryKey(),
+    url: varchar("url", { length: 1024 }).notNull(),
+    userId: varchar("user_id", { length: 256 }).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
 );
